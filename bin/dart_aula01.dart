2aea79b230e87e8c936eef32e526efda97b96382
Pedro Lima <ra-21034403-2@alunos.unicesumar.edu.br>
2023-08-05 01:24:33 +0000

    exemplos

--- bin/dart_aula01.dart
+++ bin/dart_aula01.dart
@@ -1,6 +1,29 @@
+import 'dart:ffi';
+
 import 'package:dart_aula01/dart_aula01.dart' as dart_aula01;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_aula01.calculate()}!');
-}
+  // List
+  List<String> listaCompras = ["Arroz"];
+  print(listaCompras);
+  listaCompras.add("Arroz");
 
+  // Set
+  Set<String> listaCompras2 = {"Arroz", "Feijão"};
+  listaCompras2.add("Feijão");
+
+  print(listaCompras);
+  print(listaCompras2);
+
+  // Map
+  Map<String, int> salas = {'Henrique': 13, 'Joao': 14};
+  print(salas);
+
+  var salinha = <String, int>{"Sophia": 14, "Karla": 23};
+  print(salinha);
+
+  var listaCompra = ["Arroz", "Feijão", "Carne", "Macarrão"];
+  var novaLista = ["Sal", ...listaCompra];
+
+  print(novaLista);
+}
